0cefc1721a8599c0c6392c65b79842a9a146790c
suwan <jsuwan961205@gmail.com>
2021-11-07 16:41:08 +0900

    packer add argument

--- bentoml/bento_packer.py
+++ bentoml/bento_packer.py
@@ -1,12 +1,19 @@
 from tensorflow.keras.models import load_model
 from bento_service import MnistService
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--model_path',
+    required=True,
+    type=str
+)
+args = parser.parse_args()
 
-model = load_model("gs://suwan/mnist_model_demo2/")
+model = load_model(args.model_path)
 
 mnist_svc = MnistService()
 mnist_svc.pack('model', model)
 
 saved_path = mnist_svc.save()
 print(f"saved_path : {saved_path}")
-
