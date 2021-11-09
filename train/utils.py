a861970ac7daf62cb55b19f8e73b32b0653b55ee
suwan <jsuwan961205@gmail.com>
2021-11-09 17:24:33 +0900

    Edit yq cmd

--- train/utils.py
+++ train/utils.py
@@ -3,7 +3,7 @@ import requests
 from github import Github
 
 def trigger_dispatch(ACCESS_TOKEN):
-    print("ACCESS_TOKEN: ", ACCESS_TOKEN)
+    # print("ACCESS_TOKEN: ", ACCESS_TOKEN)
     g = Github(ACCESS_TOKEN)
     g.get_user("ssuwani").get_repo("mnist-classifier-cicd").create_repository_dispatch("FINISH_TRAINING")
     
