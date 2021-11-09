c0c92c32ff3f17ee6d11d738b5bbbeca37f4da37
suwan <jsuwan961205@gmail.com>
2021-11-09 17:22:59 +0900

    show access token in while train loop for debug

--- train/utils.py
+++ train/utils.py
@@ -3,9 +3,8 @@ import requests
 from github import Github
 
 def trigger_dispatch(ACCESS_TOKEN):
-
-    github_token = ACCESS_TOKEN
-    g = Github(github_token)
+    print("ACCESS_TOKEN: ", ACCESS_TOKEN)
+    g = Github(ACCESS_TOKEN)
     g.get_user("ssuwani").get_repo("mnist-classifier-cicd").create_repository_dispatch("FINISH_TRAINING")
     
     print("Trigger CD workflow")
